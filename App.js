047563e47e0e97f9d6191c189d8a4daf33f298b6
Josh Justice <me@codingitwrong.com>
2022-02-25 17:49:30 -0500

    Theme the app

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import {StatusBar} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './src/Navigation';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#B71C1C',
+  },
+};
+
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <SafeAreaProvider>
         <StatusBar barStyle="dark-content" />
         <Navigation />
