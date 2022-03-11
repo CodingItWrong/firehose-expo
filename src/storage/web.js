b0d4bff4c76cee5a8d576b05486137edd2f19e2c
Josh Justice <me@codingitwrong.com>
2022-03-11 17:51:07 -0500

    Add web storage implementation

--- src/storage/web.js
+++ src/storage/web.js
@@ -1,9 +1,21 @@
+// WARNING: using localStorage to store keys has significant security risks.
+// It is safer to host a React app in a server-rendered webapp and store tokens
+// in cookie-secured sessions.
+
 const webStorage = {
-  setStringAsync(key, value) {},
+  setStringAsync(key, value) {
+    window.localStorage.setItem(key, value);
+    return Promise.resolve();
+  },
 
-  getStringAsync(key) {},
+  getStringAsync(key) {
+    return Promise.resolve(window.localStorage.getItem(key));
+  },
 
-  deleteStringAsync(key) {},
+  deleteStringAsync(key) {
+    window.localStorage.removeItem(key);
+    return Promise.resolve();
+  },
 };
 
 export default webStorage;
