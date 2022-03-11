a51b5c576ef4718630b9591cb10b6bb2246537db
Josh Justice <me@codingitwrong.com>
2022-03-11 17:09:25 -0500

    Make login request to server

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,9 +1,24 @@
-import {Button, TextInput} from 'react-native-paper';
+import axios from 'axios';
+import {Button, Text, TextInput} from 'react-native-paper';
+import oauthLogin from '../auth/oauthLogin';
 import useLoginForm from '../auth/useLoginForm';
 import ScreenBackground from '../components/ScreenBackground';
 
+// TODO: dynamic base URL per environment
+const baseUrl = 'http://localhost:3000';
+const path = '/api/oauth/token';
+const httpClient = axios.create({baseURL: baseUrl});
+
 export default function SignInScreen() {
-  const {username, password, handleChange} = useLoginForm();
+  const onLogIn = ({username, password}) =>
+    oauthLogin({
+      httpClient,
+      path,
+      username,
+      password,
+    }).then(token => console.log({token}));
+  const {username, password, error, handleChange, handleLogIn} =
+    useLoginForm(onLogIn);
 
   return (
     <ScreenBackground>
@@ -23,7 +38,12 @@ export default function SignInScreen() {
         onChangeText={handleChange('password')}
         secureTextEntry
       />
-      <Button mode="contained" onPress={() => {}} accessibilityLabel="Sign in">
+      <Text>{error}</Text>
+      <Button
+        mode="contained"
+        onPress={handleLogIn}
+        accessibilityLabel="Sign in"
+      >
         Sign in
       </Button>
     </ScreenBackground>
