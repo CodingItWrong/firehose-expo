7560d074c565faf69fa9a234eab88d89a025007c
Josh Justice <me@codingitwrong.com>
2022-03-18 16:23:11 -0400

    Add separate url for production api

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,10 +1,15 @@
 import {Platform} from 'react-native';
 
 function getBaseUrl() {
-  if (Platform.OS === 'android') {
-    return 'http://10.0.2.2:3000';
+  if (__DEV__) {
+    if (Platform.OS === 'android') {
+      return 'http://10.0.2.2:3000';
+    } else {
+      return 'http://localhost:3000';
+    }
   } else {
-    return 'http://localhost:3000';
+    // TODO: add custom domain before sending to App Store in case I switch off Heroku
+    return 'https://firehose-api.herokuapp.com';
   }
 }
 
