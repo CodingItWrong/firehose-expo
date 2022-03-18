e50ace0a001a26e8a076522b9474a0503322d425
Josh Justice <me@codingitwrong.com>
2022-03-18 17:21:06 -0400

    Open in new tab on web

--- src/screens/links/UnreadScreen.js
+++ src/screens/links/UnreadScreen.js
@@ -1,8 +1,17 @@
 import * as Linking from 'expo-linking';
+import {Platform} from 'react-native';
 import {List} from 'react-native-paper';
 import CenterColumn from '../../components/CenterColumn';
 import ScreenBackground from '../../components/ScreenBackground';
 import sharedStyles from '../../sharedStyles';
+
+function openBookmark(url) {
+  if (Platform.OS === 'web') {
+    window.open(url, '_blank');
+  } else {
+    Linking.openURL(url);
+  }
+}
 
 export default function UnreadScreen() {
   return (
@@ -11,7 +20,7 @@ export default function UnreadScreen() {
         <List.Item
           title="My Bookmark"
           description="codingitwrong.com"
-          onPress={() => Linking.openURL('https://codingitwrong.com')}
+          onPress={() => openBookmark('https://codingitwrong.com')}
         />
       </CenterColumn>
     </ScreenBackground>
