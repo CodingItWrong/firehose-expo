8843a133215ba00c4b538829affc46785c556f5c
Josh Justice <me@codingitwrong.com>
2022-03-18 17:12:58 -0400

    Remove placeholder screens

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,9 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Button, Text} from 'react-native-paper';
 import {large, useBreakpoint} from './breakpoints';
 import CustomNavigationBar from './components/CustomNavigationBar';
 import CustomNavigationDrawer from './components/CustomNavigationDrawer';
-import ScreenBackground from './components/ScreenBackground';
 import {useToken} from './data/token';
 import SignInScreen from './screens/SignInScreen';
 import UnreadScreen from './screens/links/UnreadScreen';
@@ -17,19 +15,6 @@ const linking = {
         initialRouteName: 'UnreadScreen',
         screens: {
           UnreadScreen: '/links/unread',
-        },
-      },
-      Hello: {
-        initialRouteName: 'HelloScreen1',
-        screens: {
-          HelloScreen1: '/hello',
-          HelloScreen2: '/hello/screen2',
-        },
-      },
-      Another: {
-        initialRouteName: 'AnotherScreen1',
-        screens: {
-          AnotherScreen1: '/another',
         },
       },
     },
@@ -51,78 +36,6 @@ const Unread = () => (
   </UnreadStack.Navigator>
 );
 
-const HelloStack = createNativeStackNavigator();
-const Hello = () => (
-  <HelloStack.Navigator
-    screenOptions={{
-      header: props => <CustomNavigationBar {...props} />,
-    }}
-  >
-    <HelloStack.Screen
-      name="HelloScreen1"
-      component={HelloScreen1}
-      options={{title: 'Hello Screen 1'}}
-    />
-    <HelloStack.Screen
-      name="HelloScreen2"
-      component={HelloScreen2}
-      options={{title: 'Hello Screen 2'}}
-    />
-  </HelloStack.Navigator>
-);
-
-function HelloScreen1() {
-  const navigation = useNavigation();
-  return (
-    <ScreenBackground>
-      <Text>Hello, screen 1!</Text>
-      <Button mode="outlined">Outline button</Button>
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate('HelloScreen2')}
-      >
-        Go to screen 2
-      </Button>
-    </ScreenBackground>
-  );
-}
-
-function HelloScreen2() {
-  return (
-    <ScreenBackground>
-      <Text>Hello, screen 2!</Text>
-    </ScreenBackground>
-  );
-}
-
-const AnotherStack = createNativeStackNavigator();
-const Another = () => (
-  <AnotherStack.Navigator
-    screenOptions={{
-      header: props => <CustomNavigationBar {...props} />,
-    }}
-  >
-    <AnotherStack.Screen
-      name="AnotherScreen1"
-      component={AnotherScreen1}
-      options={{title: 'Another!!'}}
-    />
-  </AnotherStack.Navigator>
-);
-
-function AnotherScreen1() {
-  return (
-    <ScreenBackground>
-      <Text>Another Screen</Text>
-    </ScreenBackground>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-const getDrawerTypeForBreakpoint = breakpoint =>
-  breakpoint === large ? 'permanent' : 'back';
-
 const SignInStack = createNativeStackNavigator();
 const SignIn = () => (
   <SignInStack.Navigator
@@ -137,6 +50,11 @@ const SignIn = () => (
     />
   </SignInStack.Navigator>
 );
+
+const Drawer = createDrawerNavigator();
+
+const getDrawerTypeForBreakpoint = breakpoint =>
+  breakpoint === large ? 'permanent' : 'back';
 
 function NavigationContents() {
   const {isLoggedIn} = useToken();
@@ -157,8 +75,6 @@ function NavigationContents() {
       {isLoggedIn ? (
         <>
           <Drawer.Screen name="Unread" component={Unread} />
-          <Drawer.Screen name="Hello" component={Hello} />
-          <Drawer.Screen name="Another" component={Another} />
         </>
       ) : (
         <>
