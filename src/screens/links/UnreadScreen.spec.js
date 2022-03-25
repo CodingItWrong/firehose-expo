424e5cc002a5848208ea06afed6e24cca7046c40
Josh Justice <me@codingitwrong.com>
2022-03-25 17:03:05 -0400

    Test clicking a link works

--- src/screens/links/UnreadScreen.spec.js
+++ src/screens/links/UnreadScreen.spec.js
@@ -1,9 +1,11 @@
-import {render, waitFor} from '@testing-library/react-native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import * as Linking from 'expo-linking';
 import {TokenProvider} from '../../data/token';
 import {jsonApiResponse, mockHttp} from '../../testUtils';
 import UnreadScreen from './UnreadScreen';
 
 jest.mock('../../data/authenticatedHttpClient');
+jest.mock('expo-linking', () => ({openURL: jest.fn()}));
 
 describe('UnreadScreen', () => {
   const bookmark = {
@@ -28,5 +30,20 @@ describe('UnreadScreen', () => {
     await waitFor(() => getByText(bookmark.attributes.title));
   });
 
-  it.todo('opens a link in the browser when clicked');
+  it('opens a link in the browser when clicked', async () => {
+    const http = mockHttp();
+    http.get.mockResolvedValue(jsonApiResponse([bookmark]));
+
+    const {getByText} = render(
+      <TokenProvider skipLoading>
+        <UnreadScreen />
+      </TokenProvider>,
+    );
+
+    await waitFor(() => getByText(bookmark.attributes.title));
+
+    fireEvent.press(getByText(bookmark.attributes.title));
+
+    expect(Linking.openURL).toHaveBeenCalledWith(bookmark.attributes.url);
+  });
 });
