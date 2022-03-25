8d060ae80cc61b7fa6f2aa45bac06ce4db120067
Josh Justice <me@codingitwrong.com>
2022-03-25 17:43:33 -0400

    Add toggle menus to items

--- src/screens/links/UnreadScreen.js
+++ src/screens/links/UnreadScreen.js
@@ -1,7 +1,7 @@
 import * as Linking from 'expo-linking';
 import {useEffect, useState} from 'react';
-import {FlatList, Platform} from 'react-native';
-import {List} from 'react-native-paper';
+import {FlatList, Platform, Pressable} from 'react-native';
+import {List, Menu} from 'react-native-paper';
 import CenterColumn from '../../components/CenterColumn';
 import ScreenBackground from '../../components/ScreenBackground';
 import {useBookmarks} from '../../data/bookmarks';
@@ -17,12 +17,19 @@ function openBookmark(url) {
 export default function UnreadScreen() {
   const bookmarkClient = useBookmarks();
   const [bookmarks, setBookmarks] = useState([]);
+  const [menuShownId, setMenuShownId] = useState(null);
 
   useEffect(() => {
     bookmarkClient
       .where({filter: {read: false}})
       .then(bookmarkResponse => setBookmarks(bookmarkResponse.data));
   }, [bookmarkClient]);
+
+  // TODO: test list icon a11y label
+
+  const isMenuShown = item => menuShownId === item.id;
+  const showMenu = item => setMenuShownId(item.id);
+  const hideMenu = () => setMenuShownId(null);
 
   return (
     <ScreenBackground>
@@ -35,6 +42,19 @@ export default function UnreadScreen() {
               title={item.attributes.title}
               description={item.attributes.url}
               onPress={() => openBookmark(item.attributes.url)}
+              right={props => (
+                <Menu
+                  visible={isMenuShown(item)}
+                  onDismiss={hideMenu}
+                  anchor={
+                    <Pressable onPress={() => showMenu(item)}>
+                      <List.Icon {...props} icon="dots-vertical" />
+                    </Pressable>
+                  }
+                >
+                  <Menu.Item onPress={() => {}} title="Item 1" />
+                </Menu>
+              )}
             />
           )}
         />
