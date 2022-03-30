a11ea7391ff6cb79729f8f1e366ec547e8399827
Josh Justice <me@codingitwrong.com>
2022-03-30 13:49:30 -0400

    Mock axios methods used by JSON:API library

--- src/testUtils.js
+++ src/testUtils.js
@@ -5,7 +5,9 @@ import authenticatedHttpClient from './data/authenticatedHttpClient';
 export function mockHttp() {
   const http = {
     get: jest.fn(),
+    post: jest.fn(),
     patch: jest.fn(),
+    delete: jest.fn(),
   };
   authenticatedHttpClient.mockReturnValue(http);
   return http;
