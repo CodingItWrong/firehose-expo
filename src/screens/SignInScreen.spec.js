6f888a234ad6a405b7ceb5ba10b080e14eb878c9
Josh Justice <me@codingitwrong.com>
2022-04-03 06:24:28 -0400

    Test signing in

---
+++ src/screens/SignInScreen.spec.js
@@ -0,0 +1,33 @@
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {useToken} from '../data/token';
+import {mockHttp} from '../testUtils';
+import SignInScreen from './SignInScreen';
+
+jest.mock('../data/token', () => ({useToken: jest.fn()}));
+jest.mock('../data/authenticatedHttpClient');
+
+describe('SignInScreen', () => {
+  const email = 'example@example.com';
+  const password = 'password';
+  const testToken = 'test_token';
+
+  it('sets the token upon successful login', async () => {
+    const http = mockHttp();
+    http.post.mockResolvedValue({data: {access_token: testToken}});
+    const setToken = jest.fn();
+    useToken.mockReturnValue({setToken});
+
+    const {getByLabelText, getByText} = render(<SignInScreen />);
+
+    fireEvent.changeText(getByLabelText('Email'), email);
+    fireEvent.changeText(getByLabelText('Password'), password);
+    fireEvent.press(getByText('Sign in'));
+
+    expect(http.post).toHaveBeenCalledWith('/oauth/token', {
+      grant_type: 'password',
+      username: email,
+      password,
+    });
+    await waitFor(() => expect(setToken).toHaveBeenCalledWith(testToken));
+  });
+});
