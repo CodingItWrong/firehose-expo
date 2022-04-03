5b2f832a50c9189af5aa400f0e9cb6063686d003
Josh Justice <me@codingitwrong.com>
2022-04-03 16:45:36 -0400

    Remove comment

--- src/utils/domainForUrl.js
+++ src/utils/domainForUrl.js
@@ -1,14 +1,5 @@
 import Url from 'url-parse';
 
-// const URL_REGEX = /^(\w+):\/\/([^/]*\.)?(\w+\.\w+)$/;
-
-// export default function domainForUrl(urlString) {
-//   const match = urlString.match(URL_REGEX);
-//   console.log({match});
-//   const [, _protocol, _subdomains, domain] = match;
-//   return domain;
-// }
-
 export default function domainForUrl(urlString) {
   const url = new Url(urlString);
   let host = url.hostname;
