7c46e1f424b7796562e14b3898fd20c6b58d500d
Josh Justice <me@codingitwrong.com>
2022-04-03 16:39:22 -0400

    Rename click test

--- src/screens/links/UnreadScreen.spec.js
+++ src/screens/links/UnreadScreen.spec.js
@@ -73,7 +73,7 @@ describe('UnreadScreen', () => {
   });
 
   describe('link clicking', () => {
-    it('opens a link in the browser when clicked', async () => {
+    it('opens a link in the browser when clicking the card', async () => {
       const http = mockHttp();
       http.get.mockResolvedValue(jsonApiResponse([bookmark]));
 
