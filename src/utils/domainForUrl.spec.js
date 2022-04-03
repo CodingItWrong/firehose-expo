331c451923177e59eff2f14aea4e6f0061d93e05
Josh Justice <me@codingitwrong.com>
2022-04-03 15:24:46 -0400

    Create domainForUrl function

---
+++ src/utils/domainForUrl.spec.js
@@ -0,0 +1,21 @@
+import domainForUrl from './domainForUrl';
+
+describe('domainForUrl', () => {
+  it('returns the domain for a simple url', () => {
+    expect(domainForUrl('https://google.com')).toEqual('google.com');
+  });
+
+  it('returns the domain without www for a www url', () => {
+    expect(domainForUrl('https://www.google.com')).toEqual('google.com');
+  });
+
+  it('returns the full domain for a url with subdomain', () => {
+    expect(domainForUrl('https://calendar.google.com')).toEqual(
+      'calendar.google.com',
+    );
+  });
+
+  it('returns the domain for a url with path', () => {
+    expect(domainForUrl('https://apple.com/iphone')).toEqual('apple.com');
+  });
+});
