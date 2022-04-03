a8680bdcb8a4e7dc62054e20f4f624bac2274515
Josh Justice <me@codingitwrong.com>
2022-04-03 06:31:49 -0400

    Test sign in error cases

--- src/screens/SignInScreen.spec.js
+++ src/screens/SignInScreen.spec.js
@@ -30,4 +30,39 @@ describe('SignInScreen', () => {
     });
     await waitFor(() => expect(setToken).toHaveBeenCalledWith(testToken));
   });
+
+  it('shows the returned auth error and does not set the token upon auth failure', async () => {
+    const http = mockHttp();
+    const errorMessage = 'errorMessage';
+    http.post.mockRejectedValue({
+      response: {data: {error_description: errorMessage}},
+    });
+    const setToken = jest.fn();
+    useToken.mockReturnValue({setToken});
+
+    const {findByText, getByLabelText, getByText} = render(<SignInScreen />);
+
+    fireEvent.changeText(getByLabelText('Email'), email);
+    fireEvent.changeText(getByLabelText('Password'), password);
+    fireEvent.press(getByText('Sign in'));
+
+    await findByText(errorMessage);
+    expect(setToken).not.toHaveBeenCalled();
+  });
+
+  it('shows a general error and does not set the token upon server failure', async () => {
+    const http = mockHttp();
+    http.post.mockRejectedValue();
+    const setToken = jest.fn();
+    useToken.mockReturnValue({setToken});
+
+    const {findByText, getByLabelText, getByText} = render(<SignInScreen />);
+
+    fireEvent.changeText(getByLabelText('Email'), email);
+    fireEvent.changeText(getByLabelText('Password'), password);
+    fireEvent.press(getByText('Sign in'));
+
+    await findByText('An error occurred while logging in. Please try again.');
+    expect(setToken).not.toHaveBeenCalled();
+  });
 });
