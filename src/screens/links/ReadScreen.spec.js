b745365edea12798a350899a1269211406b601c8
Josh Justice <me@codingitwrong.com>
2022-04-14 17:38:28 -0400

    Load links on Read screen

---
+++ src/screens/links/ReadScreen.spec.js
@@ -0,0 +1,57 @@
+import {useFocusEffect} from '@react-navigation/native';
+import {render} from '@testing-library/react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {TokenProvider} from '../../data/token';
+import {jsonApiResponse, mockHttp, safeAreaMetrics} from '../../testUtils';
+import ReadScreen from './ReadScreen';
+
+jest.mock('../../data/httpClient');
+jest.mock('@react-navigation/native', () => ({
+  useFocusEffect: jest.fn(),
+  useNavigation: jest.fn(),
+}));
+
+describe('ReadScreen', () => {
+  const bookmark = {
+    id: '1',
+    attributes: {
+      title: 'Test Bookmark',
+      url: 'https://www.codingitwrong.com/books',
+      comment: 'This is my book list',
+      source: 'Nice Referrer',
+      'tag-list': 'tag another-tag',
+    },
+  };
+
+  const providers = children => (
+    <SafeAreaProvider initialMetrics={safeAreaMetrics}>
+      <PaperProvider>
+        <TokenProvider skipLoading>{children}</TokenProvider>
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
+
+  beforeEach(() => {
+    // provide mock implementation of useFocusEffect to run only once
+    let effectRun = false;
+    useFocusEffect.mockImplementation(func => {
+      if (!effectRun) {
+        effectRun = true;
+        func();
+      }
+    });
+  });
+
+  describe('displaying links', () => {
+    it('renders links from the backend', async () => {
+      const http = mockHttp();
+      http.get.mockResolvedValue(jsonApiResponse([bookmark]));
+
+      const {findByText} = render(providers(<ReadScreen />));
+
+      expect(http.get).toHaveBeenCalledWith('bookmarks?filter[read]=true&');
+      await findByText(bookmark.attributes.title);
+    });
+  });
+});
--- src/screens/links/ReadScreen/index.js
+++ src/screens/links/ReadScreen/index.js
@@ -1,5 +1,14 @@
-import {Text} from 'react-native-paper';
+import {useCallback} from 'react';
+import BookmarkList from '../../../components/BookmarkList';
+import {useBookmarks} from '../../../data/bookmarks';
 
-export default function ReadScreen() {
-  return <Text>ReadScreen</Text>;
+export default function UnreadScreen() {
+  const bookmarkClient = useBookmarks();
+
+  const onLoad = useCallback(
+    () => bookmarkClient.where({filter: {read: true}}),
+    [bookmarkClient],
+  );
+
+  return <BookmarkList onLoad={onLoad} />;
 }
