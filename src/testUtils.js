e93d48e5c6d3d71f73ad390444400bed108d48b2
Josh Justice <me@codingitwrong.com>
2022-04-26 15:02:12 -0400

    Remove unused test utils

--- src/testUtils.js
+++ src/testUtils.js
@@ -1,25 +1,5 @@
 /* eslint-disable no-undef */
 
-import httpClient from './data/httpClient';
-
-// To use this function, call the following at the top level of your test:
-//
-// jest.mock('path/to/data/httpClient');
-export function mockHttp() {
-  const http = {
-    get: jest.fn(),
-    post: jest.fn(),
-    patch: jest.fn(),
-    delete: jest.fn(),
-  };
-  httpClient.mockReturnValue(http);
-  return http;
-}
-
-export function jsonApiResponse(records) {
-  return {data: jsonApiResponseBody(records)};
-}
-
 export function jsonApiResponseBody(records) {
   return {data: records};
 }
