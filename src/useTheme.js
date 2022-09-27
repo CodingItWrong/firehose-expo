688fbe41cf731155bb39660a7ca41d5d77177c4e
Josh Justice <me@codingitwrong.com>
2022-09-27 07:52:43 -0400

    Update Paper to v5

--- src/useTheme.js
+++ src/useTheme.js
@@ -1,12 +1,12 @@
 import {useColorScheme} from 'react-native';
-import {DarkTheme, DefaultTheme} from 'react-native-paper';
+import {MD2DarkTheme, MD2LightTheme} from 'react-native-paper';
 
 const FIREHOSE_RED = '#B71C1C';
 
 export default function useTheme() {
   const colorScheme = useColorScheme();
 
-  const baseTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+  const baseTheme = colorScheme === 'dark' ? MD2DarkTheme : MD2LightTheme;
 
   const theme = {
     ...baseTheme,
