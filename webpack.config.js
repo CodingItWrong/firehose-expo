598ff65865a5b227381757d2ab4b4385cf4310fd
Josh Justice <me@codingitwrong.com>
2022-11-02 07:44:32 -0400

    Attempt to transpile Reanimated

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {dangerouslyAddModulePathsToTranspile: ['reanimated']},
+    },
+    argv,
+  );
   // Customize the config before returning it.
   return config;
 };
